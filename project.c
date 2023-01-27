a7264223e4309276f2d22d9bbaa83bd0e1ec950d
N-i-T-a-M <matin.mathematics@gmail.com>
2023-01-27 17:47:49 +0330

    this is phase1 up to pastestr

--- project.c
+++ project.c
@@ -6,6 +6,82 @@
 #include <errno.h>
 
 #define S 10000
+char*content_to_copy;
+/*
+/struct clipboard{
+    char *str ;
+    struct clipboard *next;
+    struct clipboard *previous;
+};
+struct clipboard *create(){
+    struct clipboard*new1=(struct clipboard*)malloc(sizeof(struct clipboard));
+    new1->str=NULL;
+    new1->next=NULL;
+    new1->previous=NULL;
+    return new1;
+}
+void add_struct(struct clipboard *name,char *string){
+    struct clipboard*new1=(struct clipboard*)malloc(sizeof(struct clipboard));
+    while(name->next!=NULL){
+        name=name->next;
+    }
+    if(name->str==NULL){
+        name->str=string;
+    }
+    else{
+        struct clipboard *new1=create();
+        name->next=new1;
+        new1->previous=name;
+        new1->next=NULL;
+        new1->str=string;
+
+    }
+}
+*/
+int check_exist(char*address){
+    FILE*file;
+    if((file=fopen(address,"r"))==NULL){
+        printf("File doesn't exist\n");
+        return 0;
+    }
+    return 1;
+}
+int find_char(char*string,int line,int index){
+    int n=0;
+    while(*(string+n)!='\0'){
+        n++;
+    }
+    int line_count=1;
+    for(int i=0;i<n;i++){
+        if(line==line_count){
+            return i+index;
+        }
+        else if(line>line_count && *(string+i)=='\n'){
+            line_count++;
+        }
+    }
+
+}
+char* quotes(char *address1){
+    char*address=(char*)malloc(1000*sizeof(char));
+    int n=0;
+    while(*(address1+n)!='\0'){
+        n++;
+    }
+    if(*address1=='\"'){
+        for(int i=0;i<n-2;i++){
+            *(address+i)=*(address1+i+1);
+        }
+        *(address+n-2)='\0';
+        return address;
+    }
+    return address1;
+}
+   
+
+
+
+
 void createfile (char *address){//alost completely correct
     int size = 0;
     while(*(address+size)!='\0'){
@@ -52,17 +128,16 @@ void createfile (char *address){//alost completely correct
 
         }
     }
- void insertstr(char *address, char *string, int line , int index){//almost completely correct except for the ("")part
+ void insertstr(char *address1, char *string, int line , int index){//almost completely correct except for the ("")part
     /*be carefull you scan the command(insertstr) then you get --file 
     then you are given the address then --str then the string you need to 
     insert then --pos then the line of insertion then ":" and finally the
-    index which you should start insertion */
-
+    index which you should start insertion 
+    caution : if the character or the line doesn't exist the program will
+    automatically add to the closest place to the given line and index*/
+    char* address = quotes(address1);
     FILE*file;
-    if((file=fopen(address,"r"))==NULL){
-        printf("File doesn't exist\n");
-        return ;
-    }
+    if(check_exist(address)){
     
     file = fopen(address,"r");//this is the main file
     FILE*clone_file1,*clone_file2;
@@ -119,9 +194,11 @@ void createfile (char *address){//alost completely correct
     fclose(clone_file1);
     remove("root/clone1.txt");
     remove("root/clone2.txt");
-
+    }
 }
-void cat(char* address){
+void cat(char* address1){
+   char* address = quotes(address1);
+    if(check_exist(address)){
     FILE*file=fopen(address,"r");
     char*show=(char*)malloc(S*sizeof(char));
     int n=0;
@@ -130,14 +207,116 @@ void cat(char* address){
         *(show+n)=ch;
         n++;
     }
+    *(show+n)='\0';
     for(int i=0;*(show+i)!='\0';i++){
         printf("%c",*(show+i));
     }
     printf("\n");
     }
+}
+void removestr(char *address1,int line,int index,int size,char direction){
+    char* address = quotes(address1);
+    if(check_exist(address)){
+    char*content=(char*)malloc(S*sizeof(char));
+    FILE*file=fopen(address,"r");
+    char ch;
+    int n=0;
+    while((ch=fgetc(file))!=EOF){
+        *(content+n)=ch;
+        n++;
+    }
+    fclose(file);
+    // n is the size of content
+    int t = find_char(content,line,index);
+    if(direction == 'b'){//backwards
+        
+        for(int i=t;i<n;i++){
+            *(content+i-size)=*(content+i);
+        }
+    }
+    
+    else if(direction == 'f'){//forwards
+        if(t+size<n){
+        for(int i=t+size;i<n;i++){
+            *(content+i-size)=*(content+i);
+        }
+    }
+    }
+    char*content1=(char*)malloc(S*sizeof(char));
+    if(direction=='b'||t+size<n){
+    for(int i=0;i<n-size;i++){
+        *(content1+i)=*(content+i);
+    }
+    *(content1+n-size)='\0';
+    }
+    else{
+        for(int i=0;i<=t;i++){
+            *(content1+i)=*(content+i);
+        }
+        *(content1+t+1)='\0';
+    }
+    file=fopen(address,"w");
+    fprintf(file,content1);
+    fclose(file);
+}
+}
+void copystr(char *address1,int line ,int index,int size,char direction){//almost the same thing as the previous one
+/*this function got a bit complicated in addition to what they type in 
+you gotta add a struct clipboard *name to your function input
+then hopefully the part that's needed will be copied to the clipboard struct
+and it's accesible*/
+    char* address = quotes(address1);
+    if(check_exist(address)){
+    char*content=(char*)malloc(S*sizeof(char));
+    FILE*file=fopen(address,"r");
+    char ch;
+    int n=0;
+    while((ch=fgetc(file))!=EOF){
+        *(content+n)=ch;
+        n++;
+    }
+    fclose(file);
+    // n is the size of content
+    int k = find_char(content,line,index);
+    int t=k-1;
+    content_to_copy=(char*)malloc(S*sizeof(char));
+    if(direction=='b'){
+        for(int i=t-1;i>=t-size;t--){
+        *(content_to_copy+size-(t-i))=*(content+i);
+        }
+        *(content_to_copy+size)='\0';
+    }
+    else if(direction=='f'){
+        for(int i=t+1;i<=t+size;i++){
+            *(content_to_copy+i-t-1)=*(content+i);
+        }
+        *(content_to_copy+size)='\0';
+    }
+    }
+}
+void cutstr(char*address1,int line,int index,int size,char direction){
+    char* address = quotes(address1);
+    if(check_exist(address)){
+    copystr(address,line,index,size,direction);
+    removestr(address,line,index,size,direction);
+    }
+}
+void pastestr(char* address1,int line , int index){
+    char* address = quotes(address1);
+    insertstr(address,content_to_copy,line,index);
+}
 
 int main(){
-    createfile("root/file.txt");
-    insertstr("root/file.txt","salam khoobi?",1,1);
-    cat("root/file.txt");
+    //struct clipboard *init=create();//this is needed for the functions working with clipboard
+    //createfile("\"root/dir 1/file 1.txt\"");
+    //insertstr("\"root/dir 1/file 1.txt\"","salam\n",2,4);
+    //createfile("\"root/dir2/file 1.txt\"");
+    //cutstr("\"root/dir 1/file 1.txt\"",1,0,4,'f');
+    //pastestr("\"root/dir2/file 1.txt\"",1,0);
+    //cat("\"root/dir 1/file 1.txt\"");
+    //removestr("\"root/dir 1/file 1.txt\"",1,3,2,'f');
+    // cat("root/file.txt");
+    // copy("root/file1.txt",1,3,2,'b',init);
+    //cutstr("root/dir 1/file 1.txt",1,3,2,'b',init);
+    
 }
